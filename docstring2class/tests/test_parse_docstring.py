98b980ad81a2be325f52f85b94f59a2409818622
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-07-13 19:21:36 +1000

    [docstring2class/transformers.py] Implemented `docstring2ast`; fixed docstrings; changed `ast2file` implementation; [docstring2class/tests] Fixed docstrings; [docstring2class/tests/test_marshall.py] Debug and test for `test_docstring2ast`; [docstring2class/utils.py] Implemented `param2ast`, `tab`, and `simple_types`; [requirements.txt] Added typed-astunparse

--- docstring2class/tests/test_parse_docstring.py
+++ docstring2class/tests/test_parse_docstring.py
@@ -1,45 +1,32 @@
 from ast import parse
 from unittest import TestCase, main as unittest_main
 
-from meta.asttools import cmp_ast
+from meta.asttools import cmp_ast, print_ast
 
 from docstring2class.info import parse_docstring
 from docstring2class.tests.mocks import cls, ast_def, docstring0
 
 
 class TestParseDocstring(TestCase):
-    docstring0 = "\nLoad the data for your ML pipeline. Will be fed into `train`." \
-                 "\n\n:param dataset_name: name of dataset\n:type dataset_name: ```str```" \
-                 "\n\n:param data_loader: function that returns the expected data type." \
-                 "\n Defaults to TensorFlow Datasets and ml_prepare combined one." \
-                 "\n:type data_loader: ```None or (*args, **kwargs) -> tf.data.Datasets or Any```" \
-                 "\n\n:param data_loader_kwargs: pass this as arguments to data_loader function" \
-                 "\n:type data_loader_kwargs: ```**data_loader_kwargs```" \
-                 "\n\n:param data_type: incoming data type, defaults to 'infer'" \
-                 "\n:type data_type: ```str```" \
-                 "\n\n:param output_type: outgoing data_type, defaults to no conversion" \
-                 "\n:type output_type: ```None or 'numpy'```" \
-                 "\n\n:param K: backend engine, e.g., `np` or `tf`" \
-                 "\n:type K: ```None or np or tf or Any```" \
-                 "\n\n:return: Dataset splits (by default, your train and test)" \
-                 "\n:rtype: ```Tuple[np.ndarray, np.ndarray]```" \
-                 "\n"
-    docstring1 = "\nLoad the data for your ML pipeline. Will be fed into `train`." \
-                 "\n\n:param dataset_name: name of dataset\n:type dataset_name: ```str```" \
-                 "\n\n:param data_loader: function that returns the expected data type." \
-                 "\n Defaults to TensorFlow Datasets and ml_prepare combined one." \
-                 "\n:type data_loader_wrong: ```None or (*args, **kwargs) -> tf.data.Datasets or Any```" \
-                 "\n\n:param data_loader_kwargs: pass this as arguments to data_loader function" \
-                 "\n:type data_loader_kwargs: ```**data_loader_kwargs```" \
-                 "\n\n:param data_type: incoming data type, defaults to 'infer'" \
-                 "\n:type data_type: ```str```" \
-                 "\n\n:param output_type: outgoing data_type, defaults to no conversion" \
-                 "\n:type output_type: ```None or 'numpy'```" \
-                 "\n\n:param K: backend engine, e.g., `np` or `tf`" \
-                 "\n:type K: ```None or np or tf or Any```" \
-                 "\n\n:return: Dataset splits (by default, your train and test)" \
-                 "\n:rtype: ```Tuple[np.ndarray, np.ndarray]```" \
-                 "\n"
+    maxDiff = 1651
+    docstring0 = "\nLoad the data for your ML pipeline. Will be fed into `train`.\n\n" \
+                 ":param dataset_name: name of dataset\n" \
+                 ":type dataset_name: ```str```\n\n" \
+                 ":param data_loader: function that returns the expected data type." \
+                 "\n Defaults to TensorFlow Datasets and ml_prepare combined one.\n" \
+                 ":type data_loader: ```Optional[(*args, **kwargs) -> Union[tf.data.Datasets, Any]]```\n\n" \
+                 ":param data_loader_kwargs: pass this as arguments to data_loader function\n" \
+                 ":type data_loader_kwargs: ```**data_loader_kwargs```\n\n" \
+                 ":param data_type: incoming data type, defaults to 'infer'\n" \
+                 ":type data_type: ```str```\n\n" \
+                 ":param output_type: outgoing data_type, defaults to no conversion\n" \
+                 ":type output_type: ```str```\n\n" \
+                 ":param K: backend engine, e.g., `np` or `tf`\n" \
+                 ":type K: ```Optional[Union[np, tf, Any]]```\n\n" \
+                 ":return: Dataset splits (by default, your train and test)\n" \
+                 ":rtype: ```Tuple[np.ndarray, np.ndarray]```\n"
+
+    docstring1 = docstring0.replace(':type K', ':type notOK')
 
     def test_correctly_formatted(self) -> None:
         self.assertDictEqual(parse_docstring(self.docstring0),
@@ -51,8 +38,7 @@ class TestParseDocstring(TestCase):
                                                  ' Defaults to TensorFlow Datasets and ml_prepare '
                                                  'combined one.',
                                           'name': 'data_loader',
-                                          'typ': 'None or (*args, **kwargs) -> tf.data.Datasets or '
-                                                 'Any'},
+                                          'typ': 'Optional[(*args, **kwargs) -> Union[tf.data.Datasets, Any]]'},
                                          {'doc': 'pass this as arguments to data_loader function',
                                           'name': 'data_loader_kwargs',
                                           'typ': '**data_loader_kwargs'},
@@ -61,10 +47,10 @@ class TestParseDocstring(TestCase):
                                           'typ': 'str'},
                                          {'doc': 'outgoing data_type, defaults to no conversion',
                                           'name': 'output_type',
-                                          'typ': "None or 'numpy'"},
+                                          'typ': 'str'},
                                          {'doc': 'backend engine, e.g., `np` or `tf`',
                                           'name': 'K',
-                                          'typ': 'None or np or tf or Any'}],
+                                          'typ': 'Optional[Union[np, tf, Any]]'}],
                               'returns': '',
                               'short_description': 'Load the data for your ML pipeline. Will be fed into '
                                                    '`train`.'}
@@ -81,10 +67,10 @@ class TestParseDocstring(TestCase):
                                          {'doc': 'directory to look for models in. Default is '
                                                  '~/tensorflow_datasets.',
                                           'name': 'tfds_dir',
-                                          'typ': 'None or str'},
+                                          'typ': 'Optional[str]'},
                                          {'doc': 'backend engine, e.g., `np` or `tf`',
                                           'name': 'K',
-                                          'typ': 'None or np or tf or Any'},
+                                          'typ': 'Optional[Union[np, tf, Any]]'},
                                          {'doc': 'Convert to numpy ndarrays',
                                           'name': 'as_numpy',
                                           'typ': 'bool'},
@@ -100,7 +86,7 @@ class TestParseDocstring(TestCase):
     def test_badly_formatted(self) -> None:
         with self.assertRaises(AssertionError) as cte:
             parse_docstring(self.docstring1)
-        self.assertEqual('\'data_loader\' != \'data_loader_wrong\'', cte.exception.__str__())
+        self.assertEqual('\'K\' != \'notOK\'', cte.exception.__str__())
 
 
 if __name__ == '__main__':
