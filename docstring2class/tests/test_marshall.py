e8f981a4df9e43d0b6f111edb12383947a41450e
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-07-15 13:36:59 +1000

    [docstring2class/transfomers.py] Implement `ast2argparse`; [docstring2class/tests/test_marshall.py] Added test for `ast2argparse` by generalising test for `docstring2ast`; [docstring2class/tests/test_parse_docstring.py] Work with `Literals` (Python 3.8+); [docstrings2class/__init__.py] Bump version; [docstring2class/utils.py] Implemented `param2argparse_param`; [README.md,.github/workflows/main.yml] Restrict to Python 3.8 for `Literals` support

--- docstring2class/tests/test_marshall.py
+++ docstring2class/tests/test_marshall.py
@@ -3,25 +3,31 @@ from unittest import TestCase, main as unittest_main
 
 from meta.asttools import cmp_ast, str_ast
 
-from docstring2class.tests.mocks import docstring0, ast_def
-from docstring2class.transformers import docstring2ast, ast2docstring
+from docstring2class.tests.mocks import docstring0, ast_def, set_cli_func_ast
+from docstring2class.transformers import docstring2ast, ast2docstring, ast2argparse, ast2file
 
 
 class TestParseDocstring(TestCase):
+    maxDiff = 19825
+
     def test_docstring2ast(self):
         gen_ast = docstring2ast(docstring0)
-        # ast2file(gen_ast, 'delme.py', skip_black=False)
-
-        self.assertTupleEqual(*tuple(map(lambda ast: tuple(str_ast(ast).split('\n')),
-                                         (gen_ast, ast_def.body[0]))))
-        if version[:3] == '3.8':
-            self.assertTrue(cmp_ast(gen_ast, ast_def.body[0]), 'Generated AST doesn\'t match reference AST')
-        else:
-            pass
-            # self.assertTrue(cmp_ast(gen_ast, parse(cls).body[0]), 'Generated AST doesn\'t match reference AST')
+        ast2file(gen_ast, 'delme.py', skip_black=False)
+        self.run_ast_test(gen_ast)
 
     def test_ast2docstring(self) -> None:
         self.assertEqual(docstring0, ast2docstring(ast_def))
+
+    def test_ast2argparse(self) -> None:
+        gen_ast = ast2argparse(ast_def)
+        self.run_ast_test(gen_ast)
+        # ast2file(ast2argparse(ast_def), 'delme.py', skip_black=False)
+
+    def run_ast_test(self, gen_ast):
+        self.assertTupleEqual(*tuple(map(lambda ast: tuple(str_ast(ast).split('\n')),
+                                         (gen_ast, set_cli_func_ast.body[0]))))
+        if version[:3] == '3.8':
+            self.assertTrue(cmp_ast(gen_ast, ast_def.body[0]), 'Generated AST doesn\'t match reference AST')
 
 
 if __name__ == '__main__':
