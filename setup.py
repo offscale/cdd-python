acaaae54cafaec494f42913d85a46eec6de77cd5
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-07-22 00:41:16 +1000

    [README.md] Fix shield for GitHub action; [setup.py] Add docstring

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
 
+"""
+setup.py implementation, interesting because it parsed the first __init__.py and
+    extracts the `__author__` and `__version__`
+"""
+
 from ast import parse
 from distutils.sysconfig import get_python_lib
 from functools import partial
 from os import path, listdir
-from platform import python_version_tuple
 
 from setuptools import setup, find_packages
-
-if python_version_tuple()[0] == '3':
-    imap = map
-    ifilter = filter
-else:
-    from itertools import imap, ifilter
 
 if __name__ == '__main__':
     package_name = 'doctrans'
 
     with open(path.join(package_name, '__init__.py')) as f:
-        __author__, __version__ = imap(
-            lambda buf: next(imap(lambda e: e.value.s, parse(buf).body)),
-            ifilter(lambda line: line.startswith('__version__') or line.startswith('__author__'), f)
+        __author__, __version__ = map(
+            lambda buf: next(map(lambda e: e.value.s, parse(buf).body)),
+            filter(lambda line: line.startswith('__version__') or line.startswith('__author__'), f)
         )
 
     to_funcs = lambda *paths: (partial(path.join, path.dirname(__file__), package_name, *paths),
@@ -36,6 +34,6 @@ if __name__ == '__main__':
         packages=find_packages(),
         package_dir={package_name: package_name},
         data_files=[
-            (_data_install_dir(), list(imap(_data_join, listdir(_data_join()))))
+            (_data_install_dir(), list(map(_data_join, listdir(_data_join()))))
         ]
     )
