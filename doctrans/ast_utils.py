7c2735425c20c20955d16f4a5cac6e4257910be9
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-08-11 23:37:03 +1000

    Implemented and tested `emit_arg`, `RewriteAtQuery`, `annotate_ancestry`; conformed input of `find_in_ast` to match `RewriteAtQuery`

--- doctrans/ast_utils.py
+++ doctrans/ast_utils.py
@@ -1,6 +1,7 @@
 """
 ast_utils, bunch of helpers for converting input into ast.* output
 """
+import ast
 from ast import (
     AnnAssign,
     Name,
@@ -23,10 +24,12 @@ from ast import (
     NameConstant,
     Assign,
     arg,
+    AST,
 )
+from typing import Optional
 
 from doctrans.defaults_utils import extract_default
-from doctrans.pure_utils import simple_types, rpartial, PY3_8, strip_split
+from doctrans.pure_utils import simple_types, rpartial, PY3_8
 
 
 def param2ast(param):
@@ -348,13 +351,13 @@ def is_argparse_description(node):
     )
 
 
-def find_in_ast(query, node):
+def find_in_ast(search, node):
     """
     Find and return the param from within the value
 
-    :param query: Location within AST of property.
-       Can be top level like `a` for `a=5` or E.g., `A.F` for `class A: F`, `f.g` for `def f(g): pass`
-    :type query: ```str```
+    :param search: Location within AST of property.
+       Can be top level like `['a']` for `a=5` or E.g., `['A', 'F']` for `class A: F`, `['f', 'g']` for `def f(g): ...`
+    :type search: ```List[str]```
 
     :param node: AST node (must have a `body`)
     :type node: ```AST```
@@ -362,20 +365,19 @@ def find_in_ast(query, node):
     :returns: AST node that was found, or None if nothing was found
     :rtype: ```Optional[AST]```
     """
-    query_l = list(strip_split(query, "."))
     cursor = node.body
-    while len(query_l):
-        search = query_l.pop(0)
-        if len(query_l) == 0 and hasattr(node, "name") and node.name == search:
+    while len(search):
+        query = search.pop(0)
+        if len(search) == 0 and hasattr(node, "name") and node.name == query:
             return node
 
         for node in cursor:
             if isinstance(node, FunctionDef):
-                if len(query_l):
-                    search = query_l.pop(0)
+                if len(search):
+                    query = search.pop(0)
                 _cursor = next(
                     filter(
-                        lambda idx_arg: idx_arg[1].arg == search,
+                        lambda idx_arg: idx_arg[1].arg == query,
                         enumerate(node.args.args),
                     ),
                     None,
@@ -384,95 +386,118 @@ def find_in_ast(query, node):
                     if len(node.args.defaults) > _cursor[0]:
                         setattr(_cursor[1], "default", node.args.defaults[_cursor[0]])
                     cursor = _cursor[1]
-                    # print_ast(cursor)
-                    if len(query_l) == 0:
+                    if len(search) == 0:
                         return cursor
             elif (
                 isinstance(node, AnnAssign)
                 and isinstance(node.target, Name)
-                and node.target.id == search
+                and node.target.id == query
             ):
                 return node
-            elif hasattr(node, "name") and node.name == search:
-                cursor = node.body
-                break
-
-    return None
+            # elif hasattr(node, "name") and node.name == query:
+            #     cursor = node.body
+            #     break
 
 
-# def replace_in_ast(query, node, replacement_node):
-#     """
-#     Find and return the param from within the value
-#
-#     :param query: Location within AST of property.
-#        Can be top level like `a` for `a=5` or E.g., `A.F` for `class A: F`, `f.g` for `def f(g): pass`
-#     :type query: ```str```
-#
-#     :param node: AST node (must have a `body`)
-#     :type node: ```AST```
-#
-#     :param replacement_node: Replacement AST node
-#     :type replacement_node: ```AST```
-#
-#     :returns: AST node that was found, or None if nothing was found
-#     :rtype: ```Optional[AST]```
-#     """
-#     return RewriteAtQuery(query).visit(node)
-#
-#
-# class RewriteAtQuery(NodeTransformer):
-#     def __init__(self, query):
-#         self.i = 0
-#         self.query = tuple(strip_split(query, "."))
-#         self.max_i = len(self.query)
-#         self.found = [[]]
-#
-#     def visit_Module(self, node: Module):
-#         self.found[self.i].append(node.name)
-#         return node
-#
-#     def visit_ClassDef(self, node: ClassDef):
-#         self.found[self.i].append(node.name)
-#         return node
-#
-#     def visit_FunctionDef(self, node: FunctionDef):
-#         self.found[self.i].append(node.name)
-#         return node
-#
-#     def visit_arg(self, node: arg):
-#         self.found[self.i].append(node.name)
-#         pp(self.found)
-#         return node
-#
-#     def visit_arguments(self, node: arguments):
-#         self.found[self.i].append(node.name)
-#         pp(self.found)
-#         return node
-#
-#     '''
-#     def generic_visit(self, node):
-#         print(self.i, 'found node:', node, ';')
-#         if self.i < self.max_i:
-#             self.i += 1
-#             if isinstance(node, FunctionDef):
-#                 _cursor = next(
-#                     filter(
-#                         lambda idx_arg: idx_arg[1].arg == search,
-#                         enumerate(node.args.args),
-#                     ),
-#                     None,
-#                 )
-#                 if _cursor is not None:
-#                     if len(node.args.defaults) > _cursor[0]:
-#                         setattr(_cursor[1], "default", node.args.defaults[_cursor[0]])
-#                     cursor = _cursor[1]
-#                     # print_ast(cursor)
-#                     if len(query_l) == 0:
-#                         return cursor
-#             if hasattr(node, 'body'):
-#                 for _node in node.body:
-#                     self.generic_visit(_node)
-#     '''
+def annotate_ancestry(node):
+    """
+    Look to your roots. Find the child; find the parent.
+    Sets _location attribute to every child node.
+
+    :param node: AST node. Will be annotated in-place.
+    :type node: ```AST```
+    """
+    node._location = [node.name] if hasattr(node, "name") else []
+    parent_location = []
+    for _node in ast.walk(node):
+        name = [_node.name] if hasattr(_node, "name") else []
+        for child in ast.iter_child_nodes(_node):
+            if isinstance(child, FunctionDef):
+
+                def set_index(idx_arg):
+                    """
+                    :param idx_arg: Index and Any; probably out of `enumerate`
+                    :type idx_arg: ```Tuple[int, Any]```
+
+                    :returns: Second element, with _idx set with value of first
+                    :rtype: ```Any```
+                    """
+                    idx_arg[1]._idx = idx_arg[0]
+                    return idx_arg[1]
+
+                child.args.args = list(
+                    map(
+                        set_index,
+                        enumerate(
+                            child.args.args,
+                            -1
+                            if len(child.args.args) > 0
+                            and child.args.args[0].arg in frozenset(("self", "cls"))
+                            else 0,
+                        ),
+                    )
+                )
+
+            if hasattr(child, "name"):
+                child._location = name + [child.name]
+                parent_location = child._location
+            elif isinstance(child, ast.arg):
+                child._location = parent_location + [child.arg]
+
+
+class RewriteAtQuery(ast.NodeTransformer):
+    """
+    Replace the node at query with given node
+
+    :ivar search: Search query, e.g., ['class_name', 'method_name', 'arg_name']
+    :ivar replacement_node: Node to replace this search
+    """
+
+    def __init__(self, search, replacement_node, root):
+        """
+        :param search: Search query, e.g., ['class_name', 'method_name', 'arg_name']
+        :type search: ```List[str]```
+
+        :param replacement_node: Node to replace this search
+        :type replacement_node: ```ast.AST```
+
+        :param root: Root node (in case one of the visitors wants to start search over)
+        :type root: ```ast.AST```
+        """
+        self.search = search
+        self.replacement_node = replacement_node
+        self.replaced = False
+        self.root = root
+
+    def generic_visit(self, node: AST) -> Optional[AST]:
+        """
+        Visit every node, replace once, and only if found
+        """
+        if (
+            not self.replaced
+            and hasattr(node, "_location")
+            and node._location == self.search
+        ):
+            # if isinstance(node, AnnAssign):
+            #     node = emit_ann_assign(self.replacement_node)
+            # elif
+            if isinstance(node, arg):
+                value = get_value(self.replacement_node)
+                value._idx = node._idx
+                raq = RewriteAtQuery(
+                    search=self.search[:-1], root=self.root, replacement_node=value
+                )
+                raq.visit(self.root)
+                assert raq.replaced is True
+                node = emit_arg(self.replacement_node)
+            elif isinstance(node, FunctionDef) and hasattr(
+                self.replacement_node, "_idx"
+            ):
+                node.args.defaults[self.replacement_node._idx] = self.replacement_node
+            # else:
+            #    node = self.replacement_node
+            self.replaced = True
+        return ast.NodeTransformer.generic_visit(self, node)
 
 
 def emit_ann_assign(node):
@@ -493,7 +518,29 @@ def emit_ann_assign(node):
             simple=1,
             target=Name(ctx=Store(), id=node.arg),
             value=node.default if hasattr(node, "default") else None,
+            lineno=None,
+            col_offset=None,
+            end_lineno=None,
+            end_col_offset=None,
         )
+    else:
+        raise NotImplementedError(type(node).__name__)
+
+
+def emit_arg(node):
+    """
+    Produce an `arg` from the input
+
+    :param node: AST node
+    :type node: ```AST```
+
+    :returns: Something which parses to the form of `a=5`
+    :rtype: ```arg```
+    """
+    if isinstance(node, ast.arg):
+        return node
+    elif isinstance(node, AnnAssign) and isinstance(node.target, Name):
+        return arg(annotation=node.annotation, arg=node.target.id, type_comment=None,)
     else:
         raise NotImplementedError(type(node).__name__)
 
