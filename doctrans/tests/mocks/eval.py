b55d3423f625d075b6681d7506db3b335cdf08ed
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-08-12 22:46:44 +1000

    [sync_property.py] Fixed `eval` issue of non-reference by using `locals()` only

--- doctrans/tests/mocks/eval.py
+++ doctrans/tests/mocks/eval.py
@@ -2,6 +2,10 @@
 
 import doctrans.tests.mocks
 
+_attr_within = frozenset(("mocks",))
+
 get_modules = tuple(
-    attr for attr in dir(doctrans.tests) if not attr.startswith("_") and attr == "mocks"
+    attr
+    for attr in dir(doctrans.tests)
+    if not attr.startswith("_") and attr in _attr_within
 )
