d15d5feaaa3b058054253105ac629a319d9b6618
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-01-08 22:32:53 +1100

    [tests/{test_ast_utils,test_emitters,utils_for_tests,mocks/argparse}.py] Properly handle null type in argparse output
    Signed-off-by: Samuel Marks <807580+SamuelMarks@users.noreply.github.com>

--- doctrans/tests/mocks/argparse.py
+++ doctrans/tests/mocks/argparse.py
@@ -84,12 +84,13 @@ def set_cli_args(argument_parser):
         default="np",
     )
     argument_parser.add_argument(
-        "--as_numpy", type=bool, help="Convert to numpy ndarrays."
+        "--as_numpy", type=bool, help="Convert to numpy ndarrays.", default=None,
     )
     argument_parser.add_argument(
         "--data_loader_kwargs",
         type=loads,
         help="pass this as arguments to data_loader function",
+        default=None,
     )
     return argument_parser, (np.empty(0), np.empty(0))
 '''.format(
@@ -342,6 +343,11 @@ argparse_func_ast = fix_missing_locations(
                             value=set_value("Convert to numpy ndarrays."),
                             identifier=None,
                         ),
+                        keyword(
+                            arg="default",
+                            value=set_value(None),
+                            identifier=None,
+                        ),
                     ],
                     expr=None,
                     expr_func=None,
@@ -364,6 +370,11 @@ argparse_func_ast = fix_missing_locations(
                             value=set_value(
                                 "pass this as arguments to data_loader function"
                             ),
+                            identifier=None,
+                        ),
+                        keyword(
+                            arg="default",
+                            value=set_value(None),
                             identifier=None,
                         ),
                     ],
@@ -594,6 +605,11 @@ argparse_func_with_body_ast = fix_missing_locations(
                             ),
                             identifier=None,
                         ),
+                        keyword(
+                            arg="default",
+                            value=set_value(None),
+                            identifier=None,
+                        ),
                     ],
                     expr=None,
                     expr_func=None,
@@ -616,6 +632,11 @@ argparse_func_with_body_ast = fix_missing_locations(
                             value=set_value(
                                 "pass this as arguments to data_loader function",
                             ),
+                            identifier=None,
+                        ),
+                        keyword(
+                            arg="default",
+                            value=set_value(None),
                             identifier=None,
                         ),
                     ],
@@ -779,6 +800,11 @@ argparse_func_action_append_ast = fix_missing_locations(
                             value=set_value(
                                 "Collection of callables that are run inside the training loop"
                             ),
+                            identifier=None,
+                        ),
+                        keyword(
+                            arg="default",
+                            value=set_value(None),
                             identifier=None,
                         ),
                     ],
