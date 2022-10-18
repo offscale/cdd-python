ae2f89a9da54e090c19619403d60c25181c6f52e
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-10-17 21:51:29 -0500

    [setup.py] Add classifier for Python 3.10 ; [README.md] Advertise newer version of 3.11 (a5 => rc2) ; [cdd/openapi/gen_openapi.py] Update `openapi_bulk` docstring to include its return and return type ; [cdd/__init__.py] Bump version so CI/CD doesn't fail

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ def main():
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: Implementation",
             "Programming Language :: Python :: Implementation :: CPython",
             "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
