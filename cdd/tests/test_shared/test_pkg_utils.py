46085698395e296e82fe49ed6994b6b24cf12e8d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-03-15 21:39:15 -0400

    [cdd/tests/test_shared/test_pkg_utils.py] `test_get_python_lib` fix for windows?

--- cdd/tests/test_shared/test_pkg_utils.py
+++ cdd/tests/test_shared/test_pkg_utils.py
@@ -6,7 +6,7 @@ from site import getsitepackages
 from unittest import TestCase
 
 from cdd.shared.pkg_utils import get_python_lib, relative_filename
-from cdd.shared.pure_utils import rpartial, pp
+from cdd.shared.pure_utils import rpartial
 from cdd.tests.utils_for_tests import unittest_main
 
 
@@ -22,20 +22,6 @@ class TestPkgUtils(TestCase):
         """Tests that `get_python_lib` works"""
         python_lib: str = get_python_lib()
         site_packages: str = getsitepackages()[0]
-        two_dirs_above: str = path.dirname(path.dirname(site_packages))
-        pp(
-            {
-                "python_lib": python_lib,
-                "two_dirs_above": two_dirs_above,
-                "site_packages": site_packages,
-                'path.join(two_dir_above, "Lib", "site-packages")': path.join(
-                    two_dirs_above, "Lib", "site-packages"
-                ),
-                'path.join(two_dir_above, "python3", "dist-packages")': path.join(
-                    two_dirs_above, "python3", "dist-packages"
-                ),
-            }
-        )
         site_packages: str = next(
             filter(
                 rpartial(eq, python_lib),
@@ -43,7 +29,7 @@ class TestPkgUtils(TestCase):
                     lambda two_dir_above: (
                         site_packages,
                         two_dir_above,
-                        path.join(two_dir_above, "Lib", "site-packages"),
+                        path.join(site_packages, "Lib", "site-packages"),
                         path.join(two_dir_above, "python3", "dist-packages"),
                     )
                 )(path.dirname(path.dirname(site_packages))),
